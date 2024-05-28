c917af683b72e884fb15d001d0d2b4eb177dfd5e
Mohit <Mohit.Sarraf@qualitestgroup.com>
2024-05-28 21:22:56 +0530

    New Login Page with assertion

---
+++ LoginPage.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+
+namespace TestLoginProject
+{
+    class LoginPage
+    {
+        private IWebDriver driver;
+
+        public LoginPage(IWebDriver browser)
+        {
+            driver = browser;
+        }
+
+        private IWebElement UserName => driver.FindElement(By.Id("username")); 
+
+        private IWebElement Password => driver.FindElement(By.Id("password"));
+
+        private IWebElement BtnSubmit => driver.FindElement(By.Id("submit"));
+
+        private string LoggedInMsz => driver.FindElement(By.CssSelector("[class=\"post-title\"]")).Text;
+
+        private Boolean BtnLogOut => driver.FindElement(By.CssSelector("[href*=\"practice-test\"]")).Displayed;
+
+        public void loginApplication(string username, string password) 
+        {
+            UserName.SendKeys(username);
+            Console.WriteLine("Username entered successfully");
+            Password.SendKeys(password);
+            Console.WriteLine("Password entered successfully");
+            BtnSubmit.Click();
+        }
+
+        public void verifyPageUrl() 
+        {
+            string getCurrentPageUrl = driver.Url;
+            Assert.IsTrue(getCurrentPageUrl.Contains("practicetestautomation.com/logged-in-successfully/"));
+            Console.WriteLine("Print the current Url: " + getCurrentPageUrl);
+        }
+
+        public void verifyLogOutBtn()
+        {
+            Assert.IsTrue(BtnLogOut);
+        }
+
+        public void verifyLoggedInText()
+        {
+            Assert.AreEqual("Logged In Successfully", LoggedInMsz);
+            Console.WriteLine("Print the message: " + LoggedInMsz);
+        }
+
+    }
+}
---
+++ LoginTest.cs
@@ -0,0 +1,49 @@
+﻿using System.Xml.Linq;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace TestLoginProject
+{
+    internal class LoginTest
+    {
+        private IWebDriver driver;
+        private LoginPage login;
+
+        public static void Main()
+        {
+            LoginTest loginTest = new LoginTest();
+            loginTest.Setup();
+            loginTest.Login();
+            loginTest.HomePage();   
+        }
+
+        public string getTextFromXML(string value)
+        {
+            string basePath = AppDomain.CurrentDomain.BaseDirectory;
+            string xmlFilePath = Path.Combine(basePath, @"..\..\..\..\LoginProject\TestData.xml");
+
+            XDocument document = XDocument.Load(xmlFilePath);
+            var user = document.Descendants("User").FirstOrDefault();
+            return user.Element(value)?.Value;
+        }
+
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl(getTextFromXML("Url"));
+            login = new LoginPage(driver);
+        }
+
+        public void Login()
+        {
+            login.loginApplication(getTextFromXML("Username"), getTextFromXML("Password"));
+        }
+
+        public void HomePage() 
+        {
+            login.verifyPageUrl();
+            login.verifyLoggedInText();
+            login.verifyLogOutBtn();
+        }
+    }
+}
